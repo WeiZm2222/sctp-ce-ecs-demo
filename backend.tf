556505e1442a8c980f09a157027197318005d64b
zmwei <zhiminwei@gmail.com>
2023-11-14 20:59:31 +0800

    changes to names on  ecs.tf and backend-tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce3-tfstate-bucket-1"
-    key    = "ecs-fargate-sean.tfstate" # Change the value of this to yourname-tf-workspace-act.tfstate for  example
+    key    = "ecs-fargate-zmw.tfstate" # Change the value of this to yourname-tf-workspace-act.tfstate for  example. it also contains the ids that will correspond with those int he cloud. Multiple members in same team working on the code shoudl share the same statefile. 
     region = "us-east-1"
   }
 }
